813aad76723b4988e75c7da3b6698fc8454c5608
Jose Eduardo Monteiro <mds.josee@gmail.com>
2024-05-08 11:36:25 -0300

    Add deletion, mix and max

--- src/avl_tree.py
+++ src/avl_tree.py
@@ -54,6 +54,67 @@ class AVLTree:
             return self.rotate_left(root)
         return root
 
+    def delete(self, value):
+        if self.root is None:
+            return False
+        else:
+            self.root = self._delete_rec(self.root, value)
+
+    def _delete_rec(self, root, value):
+        if value < root.value:
+            root.left = self._delete_rec(root.left, value)
+        elif value > root.value:
+            root.right = self._delete_rec(root.right, value)
+        else:
+            if root.left is None:
+                tmp = root.right
+                root = None
+                return tmp
+            elif root.right is None:
+                tmp = root.left
+                root = None
+                return tmp
+            
+            tmp = self.min(root.right)
+            root.value = tmp.value
+            root.right = self._delete_rec(root.right, tmp.value)
+
+        return self.fix_delete(root)
+    
+    def fix_delete(self, root):
+        if root is None:
+            return root
+
+        self.update_height(root)
+
+        factor = self.get_factor(root)
+        if factor > 1:
+            if self.get_factor(root.left) < 0:
+                root.left = self.rotate_left(root.left)
+            return self.rotate_right(root)
+        elif factor < -1:
+            if self.get_factor(root.right) > 0:
+                root.right = self.rotate_right(root.right)
+            return self.rotate_left(root)
+        return root
+
+
+    def min(self, root):
+        curr = root
+        while curr.left is not None:
+            curr = curr.left
+        return curr
+
+    def max(self, root):
+        curr = root
+        while curr.left is not None:
+            curr = curr.left
+        return curr
+        
+
+
+
+
     def rotate_left(self, x):
         """Realizará a rotação esquerda utilizando como raiz o paramêtro `x`"""
         y = x.right
